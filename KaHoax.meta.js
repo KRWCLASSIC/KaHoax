4a845a39e652014c4bfe46fe533bb4efcd54cf18
KRW <classic.krw@gmail.com>
2025-04-01 23:10:29 +0200

    online css test and rename

---
+++ KaHoax.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         KaHoax
+// @version      1.0.25
+// @namespace    https://github.com/KRWCLASSIC
+// @description  A hack for kahoot.it!
+// @updateURL    https://github.com/KRWCLASSIC/KaHoax/raw/main/KaHoax.meta.js
+// @downloadURL  https://github.com/KRWCLASSIC/KaHoax/raw/main/KaHoax.user.js
+// @author       KRWCLASSIC, jokeri2222, johnweeky
+// @match        https://kahoot.it/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
+// @grant        none
