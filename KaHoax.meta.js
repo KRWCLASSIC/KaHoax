a999a99546d791e402e9f1113b22730f7f8a1254
KRW <classic.krw@gmail.com>
2025-04-11 23:24:32 +0200

    some little changes, gonna update icon in a moment

--- KaHoax.meta.js
+++ KaHoax.meta.js
@@ -7,5 +7,5 @@
 // @downloadURL  https://github.com/KRWCLASSIC/KaHoax/raw/main/KaHoax.user.js
 // @author       KRWCLASSIC, jokeri2222, johnweeky
 // @match        https://kahoot.it/*
-// @icon         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0id2hpdGUiPjxwYXRoIGQ9Ik0xMiAyQzYuNDg2IDIgMiA2LjQ4NiAyIDEyczQuNDg2IDEwIDEwIDEwIDEwLTQuNDg2IDEwLTEwUzE3LjUxNCAyIDEyIDJ6bTAgMThjLTQuNDExIDAtOC0zLjU4OS04LThzMy41ODktOCA4LTggOCAzLjU4OSA4IDgtMy41ODkgOC04IDh6bS0yLTEyaDR2NGgtNHYtNHptMCA2aDR2NGgtNHYtNHptLTYgMGg0djRoLTR2LTR6bTEyIDBoNHY0aC00di00eiIvPjwvc3ZnPg==
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
 // @grant        none
